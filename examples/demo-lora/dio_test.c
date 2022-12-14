015de93f8e670acc765ecc0e6e6f37219777912f
Mohamed EL BOUAZZATI <elbouazzati.mohamed.2e@gmail.com>
2022-12-14 17:39:12 +0100

    change library to libdio

--- examples/demo-lora/dio_test.c
+++ examples/demo-lora/dio_test.c
@@ -4,7 +4,7 @@
 #include "loramacnode/boards/LitexLib/defint.h"
 #include "loramacnode/boards/LitexLib/delai.h"
 #include "loramacnode/boards/LitexLib/isr.h"
-#include "loramacnode/boards/LitexLib/libisr.h"
+#include "loramacnode/boards/LitexLib/libdio.h"
 
 /**
  * @brief Test DIO0 by setting VCC to mentioned pin numbers:
@@ -16,6 +16,7 @@ void dio_test(void)
 {   
 	dio_init();
     printf("Set pin 2 or 6 or 7 or 8 to VCC to test DIO0, DIO1, DIO2, DIO3");
+
 }
 
 
