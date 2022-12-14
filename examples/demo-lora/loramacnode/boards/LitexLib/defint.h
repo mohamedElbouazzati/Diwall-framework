360709924d5224e46c85b5c26ebe875acf3dc5e8
Mohamed EL BOUAZZATI <elbouazzati.mohamed.2e@gmail.com>
2022-12-14 17:40:30 +0100

    comment already supported variables

--- examples/demo-lora/loramacnode/boards/LitexLib/defint.h
+++ examples/demo-lora/loramacnode/boards/LitexLib/defint.h
@@ -2,7 +2,7 @@
 #define DEFINT_H
 
 #ifndef _SYS__STDINT_H
-typedef unsigned char   uint8_t;
+/*typedef unsigned char   uint8_t;
 typedef unsigned short  uint16_t;
 typedef unsigned int    uint32_t;
 typedef unsigned long   uint64_t;
@@ -11,6 +11,8 @@ typedef char    int8_t;
 typedef short   int16_t;
 typedef int     int32_t;
 typedef long    int64_t;
+
+*/
 #endif
 
 #include <stdbool.h>
