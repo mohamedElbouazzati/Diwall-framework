bf2b642ea9b699395e4663f1116db4d33fafccb9
Mohamed EL BOUAZZATI <mohamed.el-bouazzati@univ-ubs.fr>
2023-06-12 10:46:15 +0200

    add Lorawan stack with CVE-2022-39274  and CVE-2020-11068

---
+++ examples/demo-loramacnode-loradawn/loramacnode/boards/LitexLib/isr.c
@@ -0,0 +1,74 @@
+
+#include <generated/csr.h>
+#include <generated/soc.h>
+#include <irq.h>
+#include <libbase/uart.h>
+#include <stdio.h>
+
+#include "isr.h"
+#include "libdio.h"
+#include "libtimer.h"
+
+
+
+void isr(void)
+{
+    __attribute__((unused)) unsigned int irqs;
+
+	irqs = irq_pending() & irq_getmask();
+
+    #ifdef CSR_TIMER1_BASE
+        #ifdef TIMER1_INTERRUPT
+            if(irqs & (1 << TIMER1_INTERRUPT))
+                timer1_isr();
+        #endif
+    #endif
+
+    #ifdef CSR_TIMER0_BASE
+        #ifdef TIMER0_INTERRUPT
+            if(irqs & (1 << TIMER0_INTERRUPT))
+                timer0_isr();
+        #endif
+    #endif
+
+    #ifdef CSR_UART_BASE
+        #ifndef UART_POLLING
+            if(irqs & (1 << UART_INTERRUPT))
+                uart_isr();
+        #endif
+    #endif
+    
+        #ifdef CSR_DIO0_BASE
+        #ifdef DIO0_INTERRUPT
+            if(irqs & (1 << DIO0_INTERRUPT))
+            {
+                dio0_isr();
+            }
+        #endif
+    #endif
+    #ifdef CSR_DIO1_BASE
+        #ifdef DIO1_INTERRUPT
+            if(irqs & (1 << DIO1_INTERRUPT))
+            {
+                dio1_isr();
+            }
+        #endif
+    #endif
+     #ifdef CSR_DIO2_BASE
+        #ifdef DIO2_INTERRUPT
+            if(irqs & (1 << DIO2_INTERRUPT))
+            {
+                dio2_isr();
+            }
+        #endif
+    #endif
+         #ifdef CSR_DIO3_BASE
+        #ifdef DIO3_INTERRUPT
+            if(irqs & (1 << DIO3_INTERRUPT))
+            {
+                dio3_isr();
+            }
+        #endif
+    #endif
+    
+}
---
+++ examples/demo-loramacnode-loradawn/loramacnode/boards/LitexLib/isr.h
@@ -0,0 +1,63 @@
+/*! -----------------------------------------------------------------------------------------------------
+ * \file      isr.h isr.c
+ *
+ * \brief     SPI implementation for Litex (loraspi)
+ *
+ * \code                                                                                                                                                                          
+ *          ___  _ __   ___ _ __                  
+ *         / _ \| '_ \ / _ \ '_ \                 
+ *        | (_) | |_) |  __/ | | |                
+ *         \___/| .__/_\___|_| |_| _ _ __ ___ ___ 
+ *              | | / __|/ _ \| | | | '__/ __/ _ \
+ *              |_| \__ \ (_) | |_| | | | (_|  __/
+ *                  |___/\___/ \__,_|_|  \___\___|
+ *                                                                                                                      
+ * \endcode
+ *
+ * \author    TERRINE Christophe ( www.ct-sylv.com )
+ * \author    Mohamed El Bouazzati ()  
+ *-------------------------------------------------------------------------------------------------------
+ * \brief TO USE THIS LIBRARY :
+ *        - define |CONFIG_CPU_HAS_INTERRUPT|
+ *        - define Your interruption like |TIMER0_SET_INTERRUPT|
+ *        - at the end you can use your interruption handler functions like |void timer0_isr(void){ // TODO }|
+ * 
+ *        TO CREATE INTERRUPTION HANDLER IRQ:
+ *        - follow this exemple statement on libirq.c :
+ *                  #ifdef TIMER0_SET_INTERRUPT                #ifdef XXXXX_SET_INTERRUPT
+ *                      if(irqs & (1 << TIMER0_INTERRUPT))          if(irqs & (1 << XXXXX_INTERRUPT))
+ *                      timer0_isr();                               XXXXX_isr();
+ *                  #endif                                      #endif
+ * 
+ *        - follow this exemple statement on libirq.h :
+ *                  void timer0_isr(void);                  void xxxxx_isr(void);
+ * 
+ *        HELP ATTENTION !! : (isr functions) are special function reconize by RISCV implementation and autogenration files 
+ * -----------------------------------------------------------------------------------------------------
+ */
+
+#ifndef __ISR_H
+#define __ISR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DIO_SET_INTERRUPT
+
+    /**
+     * @brief This function handles all interrupt transfers, you can set your irq functions here.
+     * 
+     */
+    void isr(void);
+
+    /**
+     * @brief List of interruption handler functions. 
+     * 
+     */
+ 
+#ifdef __cplusplus
+}
+#endif
+
+#endif
