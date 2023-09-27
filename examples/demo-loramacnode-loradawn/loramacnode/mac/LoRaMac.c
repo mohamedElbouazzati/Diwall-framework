872fed4bab9df5751f48bb32918f83d0b9f1114d
Mohamed EL BOUAZZATI <elbouazzati.mohamed.2e@gmail.com>
2023-09-27 11:36:25 +0200

    integrate libdiwall to loramac

--- examples/demo-loramacnode-loradawn/loramacnode/mac/LoRaMac.c
+++ examples/demo-loramacnode-loradawn/loramacnode/mac/LoRaMac.c
@@ -45,7 +45,7 @@
 #include "LoRaMacAdr.h"
 #include "LoRaMacSerializer.h"
 #include "radio.h"
-
+#include "libDiwall.h"
 #include "LoRaMac.h"
 
 /*!
@@ -745,9 +745,8 @@ static void OnRadioRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t
 {
     /*!
     * Reception buffer
-    */  //int32_t rssi2 = rssi;
-       //    int32_t rssi1 = Radio.Rssi(1);
-        //printf("rssi_radio =%d\n",(rssi1+164-(rssi1>>4)-snr));
+    */  
+   int32_t rssi2 = rssi;
     uint8_t DownlinkMaxSize = 51;
    // uint8_t RxBuffer[DownlinkMaxSize];
    uint8_t* RxBuffer = NULL; // Change to a pointer
@@ -755,8 +754,8 @@ static void OnRadioRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t
 
     uint8_t i=0;
     RxDoneParams.LastRxDone = TimerGetCurrentTime( );
-  //  printf("rssi=%d\n",rssi);
-   // HPM_enable();
+
+    HPM_enable();
     for( i = 0; i < size; i++ )
     {   
         RxBuffer[i]=*payload++;
@@ -767,9 +766,8 @@ static void OnRadioRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t
     RxDoneParams.Snr = snr;
     LoRaMacRadioEvents.Events.RxDone = 1;
     LoRaMacRadioEvents.Events.RxProcessPending = 1;
-   // __asm__ volatile("csrw 0xB05, %0 " :: "r"(rssi2));
-  // __asm__ volatile("csrw  0x320, %0\n" : : "r"(0x1F));
-   // HPM_read();
+    __asm__ volatile("csrw 0xB05, %0 " :: "r"(rssi2));
+    HPM_stop();
     OnMacProcessNotify( );
 }
 
@@ -5661,73 +5659,4 @@ LoRaMacStatus_t LoRaMacDeInitialization( void )
         return LORAMAC_STATUS_BUSY;
     }
 }
- //void HPM_read(void);
- //void HPM_stop(void);
-// void HPM_enable(void);
 
-//   void HPM_stop(void){
-//  // __asm__ volatile(".option rvc");
-//   __asm__ volatile("csrw  0x320, %0\n" : : "r"(0x1F));
-// }
-
-//   void HPM_enable(void){
-// //  __asm__ volatile(".option rvc");
-//  // set tracking by mhpmcounterX by events X           *** EVENTS ***
-//   __asm__ volatile("csrw 0x323, %0 " :: "r"(0x4)); // LD_STALL : Number of load use hazards 
-//   __asm__ volatile("csrw 0x324, %0 " :: "r"(0x8)); // JMP_STALL : Number of jump register hazards
-// //   __asm__ volatile("csrw 0x325, %0 " :: "r"(0x10)); // IMISS Cycles waiting for instruction fethces, excluding jumps and branches
-// //   __asm__ volatile("csrw 0x326, %0 " :: "r"(0x20)); // LD : Number of load instructions
-// //   __asm__ volatile("csrw 0x327, %0 " :: "r"(0x40)); // ST : Number of store instructions
-// //   __asm__ volatile("csrw 0x328, %0 " :: "r"(0x80)); // JUMP : Number of jumps(unconditional)
-// //   __asm__ volatile("csrw 0x329, %0 " :: "r"(0x100)); // BRANCH : Number of branches(conditional)
-// //   __asm__ volatile("csrw 0x32a, %0 " :: "r"(0x200)); // Branch_TAKEN : Number of branches taken (conditional)
-// //   __asm__ volatile("csrw 0x32b, %0 " :: "r"(0x400)); // COMP_INSTR : Number of compressed instructions retired
-//   //__asm__ volatile("csrw 0x32c, %0 " :: "r"(0x800)); // PIP_STALL : Cycles from stalled pipeline   
-
-// //   __asm__ volatile("csrwi 0xB00, 0x0");  // reset value of mcycle counter    
-// //   __asm__ volatile("csrwi 0xB02, 0x0");  // reset value of minstret counter
-//   __asm__ volatile("csrwi 0xB03, 0x0");  // reset value of Number of load use hazards counter 
-//   __asm__ volatile("csrwi 0xB04, 0x0");  // reset value of Number of jump register hazards counter  
-//    __asm__ volatile("csrwi 0xB05, 0x0");  // reset value of Number of Cycles waiting for instruction fethces, excluding jumps and branches counter
-// //   __asm__ volatile("csrwi 0xB06, 0x0");  // reset value of Number of load instructions counter 
-// //   __asm__ volatile("csrwi 0xB07, 0x0");  // reset value of Number of store instructions counter
-// //   __asm__ volatile("csrwi 0xB08, 0x0");  // reset value of Number of jumps(unconditional) counter
-// //   __asm__ volatile("csrwi 0xB09, 0x0");  // reset value of Number of branches(conditional) counter
-// //   __asm__ volatile("csrwi 0xB0a, 0x0");  // reset value of Number of branches taken (conditional) counter 
-// //   __asm__ volatile("csrwi 0xB0b, 0x0");  // reset value of Number of compressed instructions retired counter
-// //   __asm__ volatile("csrwi 0xB0e, 0x0");  // rssi 
-//  // enable traces
-//  __asm__ volatile("csrw  0x320, %0\n" : : "r"(0x0)); 
-// }
-//   void HPM_read(void){
-//   volatile unsigned int CYCLES=0;
-//   volatile unsigned int INSTR=0;
-//   volatile unsigned int LD_STALL=0;
-//   volatile unsigned int JMP_STALL=0;
-//   volatile unsigned int IMISS=0;
-//   volatile unsigned int LD=0;
-//   volatile unsigned int ST=0;
-//   volatile unsigned int JUMP=0;
-//   volatile unsigned int BRANCH=0;
-//   volatile unsigned int BRANCH_TAKEN=0;
-//   volatile unsigned int COMP_INSTR=0;
-//   volatile unsigned int PIPE_STALL=0;
-
-//  // __asm__ volatile(".option rvc");
-//   __asm__ volatile("csrr %0, 0xB00" : "=r"(CYCLES));
-//   __asm__ volatile("csrr %0, 0xB02" : "=r"(INSTR)); 
-//   __asm__ volatile("csrr %0, 0xB03" : "=r"(LD_STALL));
-//   __asm__ volatile("csrr %0, 0xB04" : "=r"(JMP_STALL));
-//   __asm__ volatile("csrr %0, 0xB05" : "=r"(IMISS));
-//   __asm__ volatile("csrr %0, 0xB06" : "=r"(LD));
-//   __asm__ volatile("csrr %0, 0xB07" : "=r"(ST));
-//   __asm__ volatile("csrr %0, 0xB08" : "=r"(JUMP));
-//   __asm__ volatile("csrr %0, 0xB09" : "=r"(BRANCH));
-//   __asm__ volatile("csrr %0, 0xB0a" : "=r"(BRANCH_TAKEN));
-//   __asm__ volatile("csrr %0, 0xB0b" : "=r"(COMP_INSTR));
-//   __asm__ volatile("csrr %0, 0xB0c" : "=r"(PIPE_STALL));
-
-// //printf("CYCLES,INSTR,LD_STALL,JMP_STALL,IMISS,LD,ST,JUMP,BRANCH,BRANCH_TAKEN,COMP_INSTR,PIPE_STALL\n");
-// printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",CYCLES,INSTR,LD_STALL,JMP_STALL,IMISS,LD, ST, JUMP, BRANCH,BRANCH_TAKEN,COMP_INSTR,PIPE_STALL);
-
-// }
